b29baedb908bb3584e2ccdd343c9fa2cb760dba7
OLUOKUN KABIRU ADESINA <okaathevillageboy>
2020-06-21 19:53:58 +0100

    remove admin unwanted js

--- staff/manageStudent.php
+++ staff/manageStudent.php
@@ -236,10 +236,10 @@
 
 <script>
   $(function () {
-    $('#example2').DataTable({
+    $('#example1').DataTable({
       "paging": true,
       "lengthChange": false,
-      "searching": false,
+      "searching": true,
       "ordering": true,
       "info": true,
       "autoWidth": false,
